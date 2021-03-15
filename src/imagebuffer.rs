a1bb5f7ac83b073e6159ff34d0e368d426061663
Kevin M. Gill <kmsmgill@gmail.com>
2021-03-14 17:33:30 -0700

    Using the new ImageBuffer. shift and crop are stubbed

--- src/imagebuffer.rs
+++ src/imagebuffer.rs
@@ -7,17 +7,62 @@ use image::{DynamicImage, Rgb};
 
 pub struct ImageBuffer {
     buffer: Vec<u16>,
-    width: u32,
-    height: u32,
+    pub width: u32,
+    pub height: u32,
 }
 
 impl ImageBuffer {
-    pub(crate) fn new(width:u32, height:u32) -> Self {
-        Self{buffer:Vec::with_capacity(width as usize * height as usize),
+    pub(crate) fn new(width:u32, height:u32) -> ImageBuffer {
+
+        let mut v:Vec<u16> = Vec::with_capacity(width as usize * height as usize);
+        v.resize(width as usize * height as usize, 0);
+
+        ImageBuffer{buffer:v,
             width:width,
             height:height
         }
     }
+
+    pub(crate) fn from_vec(v:Vec<u16>, width:u32, height:u32) -> ImageBuffer {
+        ImageBuffer{buffer:v,
+                    width:width,
+                    height:height
+        }
+    }
+
+    pub fn from_libraw(raw_image:&libraw::RawImage) -> ImageBuffer {
+        let top_margin:u32  = raw_image.sizes().top_margin as u32;
+        let left_margin:u32 = raw_image.sizes().left_margin as u32;
+        let h:u32 = raw_image.sizes().raw_height as u32;
+        let w:u32 = raw_image.sizes().raw_width as u32;
+
+        println!("    Raw pixel buffer height: {}", h);
+        println!("    Raw pixel buffer width: {}", w);
+        println!("    Raw pixel buffer top margin: {}", top_margin);
+        println!("    Raw pixel buffer left margin: {}", left_margin);
+
+        let need_len = (h - top_margin) * (w - left_margin);
+        println!("    Creating vector of capacity {}", need_len);
+
+        let mut v:Vec<u16> = Vec::with_capacity(need_len as usize);
+
+        for y in 0..h {
+            for x in 0..w {
+                if y >= top_margin && x >= left_margin {
+                    let idx = y * w + x;
+                    let val = raw_image[idx as usize];
+                    
+                    let put_x = (x - left_margin);
+                    let put_y = (y - top_margin);
+
+                    //println!("    Putting value into index {}", (put_y * (w-left_margin) + put_x));
+                    v.push(val);
+                }
+            }
+        }
+
+        ImageBuffer::from_vec(v, w-left_margin, h-top_margin)
+    }  
 
     pub fn get(&self, x:u32, y:u32) -> u16 {
         if x < self.width && y < self.height {
@@ -37,13 +82,24 @@ impl ImageBuffer {
         }
     }
 
+
+    pub fn crop(&self, height:u32, width:u32) -> ImageBuffer {
+
+        let mut cropped_buffer = ImageBuffer::new(width, height);
+
+        cropped_buffer.put(0, 0, 65535);
+
+        return cropped_buffer
+    }
+
+
     pub fn save(&self, to_file:&str) {
         let mut out_img = DynamicImage::new_rgb16(self.width, self.height).into_rgb16();
         
-        for y in (0..self.width) {
-            for x in (0..self.height) {
+        for y in 0..self.height {
+            for x in 0..self.width {
                 let val = self.get(x, y);
-                out_img.put_pixel(x, y, Rgb([val, 0, 0]));
+                out_img.put_pixel(x, y, Rgb([val, val, val]));
             }
         }
 
--- src/raw_to_tiff.rs
+++ src/raw_to_tiff.rs
@@ -1,6 +1,6 @@
 
 
-use crate::imagebuffer;
+use crate::imagebuffer::ImageBuffer;
 use crate::path;
 
 
@@ -23,44 +23,35 @@ struct Offset {
     v: i32,
 }
 
-fn calc_center_of_mass_offset(raw_image:&libraw::RawImage, threshold:u16) -> Offset {
+struct MinMax {
+    min: f32,
+    max: f32,
+}
+
+fn calc_center_of_mass_offset(source:&ImageBuffer, threshold:u16) -> Offset {
     let mut ox: f32 = 0.0;
     let mut oy: f32 = 0.0;
     let mut count: u32 = 0;
 
     let mut offset = Offset{h:0, v:0};
 
-
-    let top_margin:u32  = raw_image.sizes().top_margin as u32;
-    let left_margin:u32 = raw_image.sizes().left_margin as u32;
-    let h:u32 = raw_image.sizes().raw_height as u32;
-    let w:u32 = raw_image.sizes().raw_width as u32;
-
-    for y in (0..h).step_by(2) {
-        for x in (0..w).step_by(2) {
-            if y >= top_margin && x >= left_margin {
-                let idx = y * w + x;
-                let val = raw_image[idx as usize];
-                
-                if val >= threshold {
-                    ox = ox + (x as f32);
-                    oy = oy + (y as f32);
-                    count = count + 1;
-                }
-
-            }
+    for y in (0..source.height).step_by(2) {
+        for x in (0..source.width).step_by(2) {
+            let val = source.get(x, y);
+            if val >= threshold {
+                ox = ox + (x as f32);
+                oy = oy + (y as f32);
+                count = count + 1;
+            }   
         }
     }
 
     if count > 0 {
-        ox = ((w - left_margin) as f32 / 2.0) - (ox / (count as f32));
-        oy = ((h - top_margin) as f32 / 2.0) - (oy / (count as f32));
+        ox = (source.width as f32 / 2.0) - (ox / (count as f32));
+        oy = (source.height as f32 / 2.0) - (oy / (count as f32));
     }
 
-    offset.h = ox.round() as i32;
-    offset.v = oy.round() as i32;
-
-    return offset
+    Offset{h:ox.round() as i32, v:oy.round() as i32}
 }
 
 
@@ -78,98 +69,82 @@ fn linear_to_srgb(lin:f32) -> f32 {
 
 // Determined the minimum and maximum values within the 
 // red pixel channel.
-fn get_min_max(raw_image:&libraw::RawImage) -> (f32, f32) {
-
-    let top_margin:u32  = raw_image.sizes().top_margin as u32;
-    let left_margin:u32 = raw_image.sizes().left_margin as u32;
-
+fn get_min_max(source:&ImageBuffer) -> MinMax {
+    
     let mut mx:f32 = 0.0;
     let mut mn:f32 = _14_BIT_MAX as f32;
-    let h:u32 = raw_image.sizes().raw_height as u32;
-    let w:u32 = raw_image.sizes().raw_width as u32;
 
-    for y in (0..h).step_by(2) {
-        for x in (0..w).step_by(2) {
-            if y >= top_margin && x >= left_margin {
-                let idx = y * w + x;
-                let val_i32 :f32 = raw_image[idx as usize] as f32;
-                mx = if val_i32 > mx { val_i32 } else { mx };
-                mn = if val_i32 < mn { val_i32 } else { mn };
-            }
+    for y in (0..source.height).step_by(2) {
+        for x in (0..source.width).step_by(2) {
+            let val = source.get(x, y) as f32;
+            mx = if val > mx { val } else { mx };
+            mn = if val < mn { val } else { mn };
         }
     }
-
+    // Sensor dark level override.. For now...
     mn = SENSOR_DARK_LEVEL as f32;
-    return (mn, mx)
+
+    MinMax{min:mn, max:mx}
 }
 
-// Transfers from the raw image to a tiff file. Converts values from 14bit representation
-// to 16bit, normalized between supplied min & max. Only utilizes red channel, 
-// optimized for hydrogen-alpha observations, and assumes a RGGB bayer pattern.
-fn raw_image_to_image_buffer(raw_image:&libraw::RawImage, out_file : &str, mn_mx:(f32, f32)) {
-    let top_margin:u32  = raw_image.sizes().top_margin as u32;
-    let left_margin:u32 = raw_image.sizes().left_margin as u32;
 
-    let h:u32 = raw_image.sizes().raw_height as u32;
-    let w:u32 = raw_image.sizes().raw_width as u32;
+fn create_scaled_final(source:&ImageBuffer, mn_mx:&MinMax) -> ImageBuffer {
 
-    println!("    Raw pixel buffer height: {}", h);
-    println!("    Raw pixel buffer width: {}", w);
-    println!("    Raw pixel buffer top margin: {}", top_margin);
-    println!("    Raw pixel buffer left margin: {}", left_margin);
+    let dest_height = source.height / 2;
+    let dest_width = source.width / 2;
 
-    let mut out_img = DynamicImage::new_rgb16((w-left_margin)/2, (h-top_margin)/2).into_rgb16();
-    println!("    Created output buffer of width/height of {}, {}", out_img.width(), out_img.height());
+    let mut dest = ImageBuffer::new(dest_width, dest_height);
 
-    for y in (0..h).step_by(2) {
-        for x in (0..w).step_by(2) {
-            if y >= top_margin && x >= left_margin {
-                let idx = y * w + x;
-                let mut val_f32 :f32 = raw_image[idx as usize] as f32;
+    for y in (0..source.height).step_by(2) {
+        for x in (0..source.width).step_by(2) {
+            let put_x = x / 2;
+            let put_y = y / 2;
+            
+            let mut val_f32 :f32 = source.get(x, y) as f32;
 
-                val_f32 = (val_f32 - mn_mx.0) / (mn_mx.1 - mn_mx.0) * (_16_BIT_MAX as f32);
-                
-                //val_f32 = linear_to_srgb(val_f32);
+            val_f32 = (val_f32 - mn_mx.min) / (mn_mx.max - mn_mx.min) * (_16_BIT_MAX as f32);
+            //val_f32 = linear_to_srgb(val_f32);
+            let val:u16 = val_f32.round() as u16;
 
-                let val:u16 = val_f32.round() as u16;
-
-                let put_x = (x - left_margin) / 2;
-                let put_y = (y - top_margin) / 2;
-
-                if put_x < out_img.width() && put_y < out_img.height() {
-                    out_img.put_pixel(put_x, put_y, Rgb([val, 0, 0]));
-                } 
-            }
+            dest.put(put_x, put_y, val);
         }
     }
 
-    println!("    Writing image buffer to file");
-    if path::parent_exists_and_writable(&out_file) {
-        out_img.save(out_file).unwrap();
-    } else {
-        eprintln!("Parent does not exist or cannot be written: {}", path::get_parent(out_file));
-        process::exit(1);
-    }
+    return dest
 }
 
-// Processes an input CR2 raw image file (Canon EOS)
-pub fn process(raw_file:&str) {
+
+
+fn load_raw_image(raw_file:&str) -> ImageBuffer {
     println!("    Reading raw image file {}", raw_file);
     let buf = fs::read(raw_file).expect("read in");
 
     println!("    Decoding for raw pixel values");
     let processor = libraw::Processor::new();
     let raw_image = processor.decode(&buf).unwrap();
+    ImageBuffer::from_libraw(&raw_image)
+}
+
+// Processes an input CR2 raw image file (Canon EOS)
+pub fn process(raw_file:&str) {
+
+    let source = load_raw_image(raw_file);
+
 
     let out_file = raw_file.replace("CR2", "tif").replace("cr2", "tif");
     println!("    Determined output file path to be {}", out_file);
 
-    let mn_mx = get_min_max(&raw_image);
-    println!("    Pixel Min/Max (Red only): {}, {}", mn_mx.0, mn_mx.1);
+    let mn_mx = get_min_max(&source);
 
-    let offset = calc_center_of_mass_offset(&raw_image, 1023);
+    println!("    Pixel Min/Max (Red only): {}, {}", mn_mx.min, mn_mx.max);
+
+    let offset = calc_center_of_mass_offset(&source, SENSOR_DARK_LEVEL);
+
     println!("    Horizonal center of Mass Offset: {}", offset.h);
     println!("    Vertical Center of Mass Offset: {}", offset.v);
 
-    raw_image_to_image_buffer(&raw_image, &out_file, mn_mx);
+    let scaled = create_scaled_final(&source, &mn_mx);
+
+    scaled.save(&out_file);
+    //raw_image_to_image_buffer(&raw_image, &out_file, mn_mx);
 }
